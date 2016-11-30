bb5c8f901144d995b9f4fe242ebb0d80a5467fee
pjwilson27 <pjwilson27@gmail.com>
2016-11-30 18:44:33 -0500

    fixing error with play/pause
    seemed that i was missing the . within the ()

--- scripts/album.js
+++ scripts/album.js
@@ -42,11 +42,11 @@ var createSongRow = function(songNumber, songName, songLength) {
 
 var setCurrentAlbum = function(album) {
     //#1
-    var albumTitle = document.getElementsByClassName('album-view-title') [0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+    var albumTitle = document.getElementsByClassName('.album-view-title') [0];
+    var albumArtist = document.getElementsByClassName('.album-view-artist')[0];
+    var albumReleaseInfo = document.getElementsByClassName('.album-view-release-info')[0];
+    var albumImage = document.getElementsByClassName('.album-cover-art')[0];
+    var albumSongList = document.getElementsByClassName('.album-view-song-list')[0];
     
     //2
     albumTitle.firstChild.nodeValue = album.title;
