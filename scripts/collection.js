e2a25b009a10f626b95c1f61e186176088e5a55e
Richard Newman <richard@newmanworks.com>
2016-12-06 18:01:48 -0700

    fixed collection.js for ckpt

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,6 +1,7 @@
 var buildCollectionItemTemplate = function() {
-    var template =
-
+  var template =
+    '<div class="collection-album-container column fourth">' +
+    '  <img src="assets/images/album_covers/01.png"/>' +
     '  <div class="collection-album-info caption">' +
     '    <p>' +
     '      <a class="album-name" href="album.html"> The Colors </a>' +
@@ -12,12 +13,9 @@ var buildCollectionItemTemplate = function() {
     '    </p>' +
     '  </div>' +
     '</div>'
-    ;
-return $(template);
-
+  ;
+  return $(template);
 };
-
-
 
 $(window).load(function() {
     //#1
@@ -26,11 +24,9 @@ $(window).load(function() {
     //#3
     var $collectionContainer = $('.album-covers');
     $collectionContainer.empty();
-    
+
     for (var i = 0; i < 12; i++) {
         var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
     }
-    
-    $collectionContainer.append($newThumbnail);
-
 });
